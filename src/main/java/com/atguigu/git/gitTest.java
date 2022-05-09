dd61a022aff254801a8ec4f063c1851c94425c74
HXD <HXD@qq.com>
2022-05-09 17:26:49 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @Author HXD
+ * @Description:
+ * @Date: Created in 17:18 2022/5/9
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
